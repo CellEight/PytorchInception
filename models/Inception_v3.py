737f1d67c977eb0fb9d75031280fbaade400c1c3
Lewis Patten <lp921@york.ac.uk>
2021-03-12 17:19:20 +0000

    Finished models, now to debug

---
+++ models/Inception_v3.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utility.Inception import *
+
+class Incpetion_v3(nn.Module):
+    def __init__(self):
+        """ A Pytorch implementation of the inception v3 architecture decibed in the paper
+            "Rethinking the Inception Architecture for Computer Vision"  by Szegedy et al.""" 
+        super().__init__()
+        self.conv1 = nn.Conv2d(3,32,kernel_size=3,stride=2)
+        self.conv2 = nn.Conv2d(32,32,kernel_size=3,stride=1)
+        self.conv3 = nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1,padding_mode='reflect')
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.conv4 = nn.Conv2d(64,80,kernel_size=3,stride=1)
+        self.conv5 = nn.Conv2d(80,192,kernel_size=3,stride=2)
+        self.conv5 = nn.Conv2d(192,288,kernel_size=3,stride=1,padding=1,padding_mode='reflect')
+        self.inca1 = FactorIncpetionA(288,192,(235,192),(235,192),192)
+        self.inca2 = FactorIncpetionA(768,192,(192,192),(192,192),192)
+        self.inca3 = FactorIncpetionA(768,192,(192,192),(192,192),192)
+        self.incb1 = FactorIncpetionB(768,320,(480,320),(480,320),320)
+        self.incb2 = FactorIncpetionB(1280,320,(320,320),(320,320),320)
+        self.incb3 = FactorIncpetionB(1280,320,(320,320),(320,320),320)
+        self.incb4 = FactorIncpetionB(1280,320,(320,320),(320,320),320)
+        self.incb5 = FactorIncpetionB(1280,320,(320,320),(320,320),320)
+        self.incc1 = FactorIncpetionC(1280,512,(768,512),(768,512),512)
+        self.incc2 = FactorIncpetionC(2048,512,(512,512),(512,512),512)
+        self.pool2 = nn.MaxPool2d(kernel_size=8)
+        self.fc1   = nn.Linear(2048,1000)
+
+    def forward(self,x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.inca1(x)
+        x = self.inca2(x)
+        x = self.inca3(x)
+        x = self.incb1(x)
+        x = self.incb1(x)
+        x = self.incb2(x)
+        x = self.incb3(x)
+        x = self.incb4(x)
+        x = self.incb5(x)
+        x = self.incc1(x)
+        x = self.incc2(x)
+        x = self.pool2(x)
+        x = self.fc1(x)
+        return x
--- utility/Inception.py
+++ utility/Inception.py
@@ -1,9 +1,176 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from math import sqrt, floor
 
-class Inception(nn.Module):
-    """ An implementation of the Inception blocks desired in the GoogLeNet Paper """
+class GoogLeNetInception(nn.Module):
+    """ An implementation of the Inception block described in the GoogLeNet Paper """
+    def __init__(self, dim_in, dim_1, dims_3, dims_5, dim_pool):
+        super().__init__()
+        # 1x1 Convolution Path
+        self.redu1 = nn.Conv2d(dim_in, dim_1, kernel_size=1, stride=1) 
+        # 3x3 Convolution Path
+        self.redu2 = nn.Conv2d(dim_in, dims_3[0], kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(dims_3[0], dims_3[1], kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        # 5x5 Convolution Path
+        self.redu3 = nn.Conv2d(dim_in, dims_5[0], kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(dims_5[0], dims_5[1], kernel_size=5, stride=1, padding=2, padding_mode='reflect')
+        # Max-Pool Pat
+        self.pool  = nn.MaxPool2d(3,1, padding=1)
+        self.redu4 = nn.Conv2d(dim_in, dim_pool, kernel_size=1,stride=1)
+
+    def forward(self,x):
+        # 1x1 Convolution Path
+        x1 = F.relu(self.redu1(x))
+        
+        # 3x3 Convolution Path
+        x2 = F.relu(self.redu2(x))
+        x2 = F.relu(self.conv1(x2))
+        # 5x5 Convolution Path
+        x3 = F.relu(self.redu3(x))
+        x3 = F.relu(self.conv2(x3))
+        
+        # Max-Pool Path
+        x4 = self.pool(x)
+        x4 = F.relu(self.redu4(x4))
+        
+        #  Concatenate the resulting tensors
+        x = torch.cat((x1,x2,x3,x4),1) 
+        return x
+
+class FactorInceptionA(nn.Module):
+    """ An implementation of the Factorized Inception block described in Figure 5 
+        of the paper "Rethinking the Inception Architecture for Computer Vision" """
+    def __init__(self, dim_in, dim_1, dims_3, dims_5, dim_pool):
+        super().__init__()
+        # 1x1 Convolution Path
+        self.redu1 = nn.Conv2d(dim_in, dim_1, kernel_size=1, stride=1) 
+        # 3x3 Convolution Path
+        self.redu2 = nn.Conv2d(dim_in, dims_3[0], kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(dims_3[0], dims_3[1], kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        # 5x5 Convolution Path
+        self.redu3 = nn.Conv2d(dim_in, dims_5[0], kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(dims_5[0], dims_5[1], kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.conv3 = nn.Conv2d(dims_5[1], dims_5[1], kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        # Max-Pool Pat
+        self.pool  = nn.MaxPool2d(3,1, padding=1)
+        self.redu4 = nn.Conv2d(dim_in, dim_pool, kernel_size=1,stride=1)
+
+    def forward(self,x):
+        # 1x1 Convolution Path
+        x1 = F.relu(self.redu1(x))
+        
+        # 3x3 Convolution Path
+        x2 = F.relu(self.redu2(x))
+        x2 = F.relu(self.conv1(x2))
+        # 5x5 Convolution Path
+        x3 = F.relu(self.redu3(x))
+        x3 = F.relu(self.conv2(x3))
+        x3 = F.relu(self.conv3(x3))
+        
+        # Max-Pool Path
+        x4 = self.pool(x)
+        x4 = F.relu(self.redu4(x4))
+        
+        #  Concatenate the resulting tensors
+        x = torch.cat((x1,x2,x3,x4),1) 
+        return x
+
+class FactorInceptionB(nn.Module):
+    """ An implementation of the Factorized Inception blocks described in Figure 6 
+        of the paper "Rethinking the Inception Architecture for Computer Vision" """
+    def __init__(self, dim_in, dim_1, dims_3, dims_5, dim_pool, n):
+        super().__init__()
+        # 1x1 Convolution Path
+        self.redu1 = nn.Conv2d(dim_in, dim_1, kernel_size=1, stride=1) 
+        
+        # nxn Convolution Path
+        self.redu2 = nn.Conv2d(dim_in, dims_3[0], kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(dims_3[0], dims_3[1], kernel_size=(1,n), stride=1, padding=1, padding_mode='reflect')
+        self.conv2 = nn.Conv2d(dims_3[0], dims_3[1], kernel_size=(n,1), stride=1, padding=1, padding_mode='reflect')
+        
+        # ?x? Convolution Path
+        self.redu3 = nn.Conv2d(dim_in, dims_5[0], kernel_size=1, stride=1)
+        self.conv3 = nn.Conv2d(dims_3[0], dims_3[1], kernel_size=(1,n), stride=1, padding=1, padding_mode='reflect')
+        self.conv4 = nn.Conv2d(dims_3[1], dims_3[1], kernel_size=(n,1), stride=1, padding=1, padding_mode='reflect')
+        self.conv5 = nn.Conv2d(dims_3[1], dims_3[1], kernel_size=(1,n), stride=1, padding=1, padding_mode='reflect')
+        self.conv6 = nn.Conv2d(dims_3[1], dims_3[1], kernel_size=(n,1), stride=1, padding=1, padding_mode='reflect')
+        
+        # Max-Pool Pat
+        self.pool  = nn.MaxPool2d(3,1, padding=1)
+        self.redu4 = nn.Conv2d(dim_in, dim_pool, kernel_size=1,stride=1)
+
+    def forward(self,x):
+        # 1x1 Convolution Path
+        x1 = F.relu(self.redu1(x))
+        
+        # nxn Convolution Path
+        x2 = F.relu(self.redu2(x))
+        x2 = F.relu(self.conv1(x2))
+        x2 = F.relu(self.conv2(x2))
+
+        # ?x? Convolution Path
+        x3 = F.relu(self.redu3(x))
+        x3 = F.relu(self.conv3(x3))
+        x3 = F.relu(self.conv4(x3))
+        x3 = F.relu(self.conv5(x3))
+        x3 = F.relu(self.conv6(x3))
+
+        # Max-Pool Path
+        x4 = self.pool(x)
+        x4 = F.relu(self.redu4(x4))
+        
+        #  Concatenate the resulting tensors
+        x = torch.cat((x1,x2,x3,x4),1) 
+        return x
+
+class FactorInceptionC(nn.Module):
+    """ An implementation of the Factorized Inception blocks described in Figure 7 
+        of the paper "Rethinking the Inception Architecture for Computer Vision" """
+    def __init__(self, dim_in, dim_1, dims_3, dims_5, dim_pool):
+        super().__init__()
+        # 1x1 Convolution Path
+        self.redu1 = nn.Conv2d(dim_in, dim_1, kernel_size=1, stride=1) 
+        # 3x3 Convolution Path 
+        self.redu3 = nn.Conv2d(dim_in, dims_3[0], kernel_size=1, stride=1)
+        self.conv1 = nn.Conv2d(dims_3[0], dims_3[1]//2, kernel_size=(1,3), stride=1, padding=1, padding_mode='reflect')
+        self.conv2 = nn.Conv2d(dims_3[0], dims_3[1]//2, kernel_size=(3,1), stride=1, padding=1, padding_mode='reflect')
+        # 5x5 Convolution Path
+        
+        self.redu2 = nn.Conv2d(dim_in, dims_5[0], kernel_size=1, stride=1)
+        self.conv3 = nn.Conv2d(dims_5[0], dims_5[1], kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.conv4 = nn.Conv2d(dims_5[1], dims_5[1]//2, kernel_size=(1,3), stride=1, padding=1, padding_mode='reflect')
+        self.conv5 = nn.Conv2d(dims_5[1], dims_5[1]//2, kernel_size=(3,1), stride=1, padding=1, padding_mode='reflect')
+        # Max-Pool Pat
+        self.pool  = nn.MaxPool2d(3,1, padding=1)
+        self.redu4 = nn.Conv2d(dim_in, dim_pool, kernel_size=1,stride=1)
+
+    def forward(self,x):
+        # 1x1 Convolution Path
+        x1 = F.relu(self.redu1(x))
+        
+        # 3x3 Convolution Path
+        x2 = F.relu(self.redu2(x))
+        x2a = F.relu(self.conv1(x2))
+        x2b = F.relu(self.conv2(x2))
+
+        # 5x5 Convolution Path
+        x3 = F.relu(self.redu3(x))
+        x3 = F.relu(self.conv3(x3))
+        x3a = F.relu(self.conv4(x3))
+        x3b = F.relu(self.conv5(x3))
+        
+        # Max-Pool Path
+        x4 = self.pool(x)
+        x4 = F.relu(self.redu4(x4))
+        
+        #  Concatenate the resulting tensors
+        x = torch.cat((x1,x2a,x2b,x3a,x3b,x4),1) 
+        return x
+
+class FactorInception4(nn.Module):
+    """ An implementation of the Factorized Inception blocks described in Figure 5 
+        of the paper "Rethinking the Inception Architecture for Computer Vision" """
     def __init__(self, dim_in, dim_1, dims_3, dims_5, dim_pool):
         super().__init__()
         # 1x1 Convolution Path
